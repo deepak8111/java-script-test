5a8fc3fb932314f408a10d7a9cf0943e34ba0e0e
Deepak Yadav <168839873+deepak8111@users.noreply.github.com>
2024-05-13 12:04:53 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "hitesh"
+let score = "deepak"
 
 //console.log(typeof score);
 //console.log(typeof(score));
@@ -12,14 +12,14 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "deepak"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
-// "hitesh" => true
+// "deepak" => true
 
 let someNumber = 33
 
